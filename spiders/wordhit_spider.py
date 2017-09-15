59226f3d8db9f44dee605cae380db409618d5ab9
Holanda Junior <joaoholandajr@gmail.com>
2017-09-15 07:42:18 -0300

    Refactoring spider

--- spiders/wordhit_spider.py
+++ spiders/wordhit_spider.py
@@ -4,12 +4,19 @@ from scrapy.selector import Selector
 class WordHitSpider(scrapy.Spider):
 	name = 'wordhitSpider'
 	allowed_domains = ["www.bing.com"]
-	start_urls = ['https://www.google.com.br/search?q=teste&oq=teste', 'https://www.google.com.br/search?q=teste&oq=teste']
-
+	
 	custom_settings = {
         'DOWNLOAD_DELAY': 4,
         'CONCURRENT_REQUESTS': 1
     }
+
+	def start_requests(self):
+		urls = [
+			'https://www.google.com.br/search?q=teste', 
+			'https://www.google.com.br/search?q=project']
+
+		for url in urls:
+			yield scrapy.Request(url=url, callback=self.parse)
 
 	def parse(self, response):
 		hitsText = Selector(response).xpath('//*[@id="resultStats"]/text()').extract()[0]
