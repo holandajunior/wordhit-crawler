adf63b7158aa9ae51a92ea8a7c81bf678ce6bdd3
Holanda Junior <joaoholandajr@gmail.com>
2017-09-21 07:29:59 -0300

    Configuring project to work with pipelines

--- wordhit_crawler/settings.py
+++ wordhit_crawler/settings.py
@@ -1,8 +1,8 @@
-def load_lines(path):
-	with open(path, 'rb') as f:
-		return [line.strip() for line in
-			f.read().decode('utf8').splitlines()
-			if line.strip()]
+# def load_lines(path):
+# 	with open(path, 'rb') as f:
+# 		return [line.strip() for line in
+# 			f.read().decode('utf8').splitlines()
+# 			if line.strip()]
 
 # ROTATING_PROXY_LIST = load_lines('proxies.txt') 
 # ROTATING_PROXY_PAGE_RETRY_TIMES = 1
@@ -15,6 +15,19 @@ DOWNLOADER_MIDDLEWARES = {
 	'random_useragent.RandomUserAgentMiddleware': 400
 }
 
+# Select user-agents randomnly
 USER_AGENT_LIST = "user-agents.txt"
 
+# Just bot name
 BOT_NAME = 'wordhit'
+
+# Defining pipelines
+ITEM_PIPELINES = {
+	'wordhit_crawler.pipelines.MongoPipeline': 300}
+
+# Mongo configs
+MONGODB_SERVER = '172.17.0.2'
+MONGODB_PORT = 27017
+MONGODB_DB = 'opiniorizer'
+MONGODB_COLLECTION = 'words'
+
